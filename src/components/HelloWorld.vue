7789d22416e83c9e8e5131900724ba9c6c453ca9
Doomsquard <zhilinsky99@gmail.com>
2021-02-20 13:34:09 +0300

    lesson 3 , filters img

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,148 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="root">
+		<div class="img">
+			<img
+				v-if="showImage"
+				src="../assets/img/aIdDy6e2Bdg.jpg"
+				:style="currentImageSize"
+				:class="filterImg"
+			/>
+			<p v-else>come back soon</p>
+		</div>
+		<form>
+			<label for="rangeHeight">height:{{ sizeImage.currentHeight }}</label>
+			<input
+				type="range"
+				:min="sizeImage.minHeight"
+				id="rangeHeight"
+				:max="sizeImage.maxHeight"
+				:value="sizeImage.currentHeight"
+				@input="sizeImage.currentHeight = $event.target.value"
+			/>
+		</form>
+		<form>
+			<label for="rangeWidth">width:{{ sizeImage.currentWidth }}</label>
+			<input
+				type="range"
+				:min="sizeImage.minWidth"
+				id="rangeWidth"
+				:max="sizeImage.maxWidth"
+				:value="sizeImage.currentWidth"
+				@input="sizeImage.currentWidth = $event.target.value"
+			/>
+		</form>
+		<form>
+			<label for="rotate">rotate:{{ sizeImage.rotate.currentDeg }}</label>
+			<input
+				type="range"
+				:min="sizeImage.rotate.minDeg"
+				id="rotate"
+				:max="sizeImage.rotate.maxDeg"
+				:value="sizeImage.rotate.currentDeg"
+				@input="sizeImage.rotate.currentDeg = $event.target.value"
+			/>
+		</form>
+		<button @click="showImage = !showImage">{{ buttonMsg }}</button>
+		<div class="filters">
+			<h2>Фильтры</h2>
+			<button
+				@click="filterImg.sepia = !filterImg.sepia"
+				:class="filterImg.sepia ? 'active' : ''"
+			>
+				Сепия
+			</button>
+			<button
+				@click="filterImg.blur = !filterImg.blur"
+				:class="filterImg.blur ? 'active' : ''"
+			>
+				Размытие
+			</button>
+			<button
+				@click="filterImg.shadows = !filterImg.shadows"
+				:class="filterImg.shadows ? 'active' : ''"
+			>
+				Тени
+			</button>
+			<button
+				@click="filterImg.border = !filterImg.border"
+				:class="filterImg.border ? 'active' : ''"
+			>
+				Рамка
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				showImage: true,
+				sizeImage: {
+					minHeight: 300,
+					maxHeight: 500,
+					minWidth: 300,
+					maxWidth: 500,
+					currentHeight: 500,
+					currentWidth: 500,
+					rotate: {
+						minDeg: 0,
+						maxDeg: 360,
+						currentDeg: 0,
+					},
+				},
+				filterImg: {
+					sepia: false,
+					blur: false,
+					shadows: false,
+					border: false,
+				},
+			}
+		},
+		computed: {
+			currentImageSize: function() {
+				return {
+					height: `${this.sizeImage.currentHeight}px`,
+					width: `${this.sizeImage.currentWidth}px`,
+					transform: `rotate(${this.sizeImage.rotate.currentDeg}deg)`,
+				}
+			},
+			buttonMsg: function() {
+				return this.showImage ? 'Скрыть' : 'Показать'
+			},
+		},
+	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	.img {
+		min-width: 500px;
+		min-height: 500px;
+	}
+	button {
+		min-height: 30px;
+		min-width: 100px;
+	}
+	.filters {
+		display: flex;
+		padding: 10px;
+		align-items: center;
+		justify-content: center;
+	}
+	.sepia {
+		filter: sepia(60%);
+	}
+	.blur {
+		filter: blur(10px);
+	}
+	.shadows {
+		box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);
+	}
+	.border {
+		border: 4px solid black;
+	}
+	.active {
+		background-color: grey;
+	}
 </style>
